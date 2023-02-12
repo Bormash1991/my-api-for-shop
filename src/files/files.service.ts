ef0686d75fee682267d221bd7cd88e3159a8f1e8
Bormash1991 <53615723+Bormash1991@users.noreply.github.com>
2023-02-13 01:21:14 +0200

    resovled bugs

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -6,16 +6,16 @@ import * as fs from 'fs';
 export class FilesService {
   async createFiles(files: any[]) {
     try {
-    const filePath = path.join(__dirname, '..', 'static');
-    console.log(filePath);
-    if (!fs.existsSync(filePath)) {
-      fs.mkdir(filePath, { recursive: true }, () => {});
-    }
-    return files.map((file) => {
-      const fileName = `${uuidv4()}.${file.mimetype.split('/')[1]}`;
-      fs.writeFile(path.join(filePath, fileName), file.buffer, (error) => {});
-      return fileName;
-    });
+      const filePath = path.join(__dirname, '..', 'static');
+
+      if (!fs.existsSync(filePath)) {
+        fs.mkdir(filePath, { recursive: true }, () => {});
+      }
+      return files.map((file) => {
+        const fileName = `${uuidv4()}.${file.mimetype.split('/')[1]}`;
+        fs.writeFile(path.join(filePath, fileName), file.buffer, (error) => {});
+        return fileName;
+      });
     } catch (error) {
       throw new HttpException(
         'error occurred while writing the file',
