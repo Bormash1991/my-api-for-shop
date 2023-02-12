cacddfac9c679ceab5ffa4f5af3b185ee6eb8983
Bormash1991 <53615723+Bormash1991@users.noreply.github.com>
2023-02-13 01:11:01 +0200

    changed vercel.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,6 @@ import { MulterModule } from '@nestjs/platform-express';
     }),
     ServeStaticModule.forRoot({
       rootPath: path.join(__dirname, 'static'),
-      serveRoot: '/static',
     }),
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.${process.env.DB_HASH}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
